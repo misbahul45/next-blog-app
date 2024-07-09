bd04cf681cc5dc0713f1caf5aacfb9040247faad
misbahul45 <misbahulmuttaqin395@example.com>
2024-07-09 12:07:39 +0700

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import fotoU from "@/public/fotou.jpg"
 import Image from 'next/image'
+import { BASE_URL } from '@/fetch/url'
 
 const page = () => {
+  if(!BASE_URL){
+    return null
+  }
   return (
     <section className='h-[calc(100vh-4rem)] flex flex-col items-center justify-center'>
       <iframe src="https://misbahul-cv.vercel.app/" className='w-full h-full border-0' />
